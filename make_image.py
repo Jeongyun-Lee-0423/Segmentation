fec6c28c7fde5b826509b2b615bb9e88a6a465d3
Jeongyun-Lee-0423 <62532988+Jeongyun-Lee-0423@users.noreply.github.com>
2020-03-23 18:50:22 +0900

    Update make_image.py

--- make_image.py
+++ make_image.py
@@ -17,12 +17,12 @@ for image_num in range(0,100):
     data = np.reshape(data, (256,256))
     label_data = np.zeros((256,256), dtype = np.uint8)
     
-    for defect_num in range(1,6):
+    for defect_num in range(1,3):
         size_x = np.random.randint(8,64, size = 1 , dtype = np.uint8)
         size_y = np.random.randint(8,64, size = 1 , dtype = np.uint8)
         x = np.random.randint(0,256-size_x[0], size = 1 , dtype = np.uint8)
         y = np.random.randint(0,256-size_y[0], size = 1 , dtype = np.uint8)
-        defect_contrast = np.random.randint(4,7, size = 1 , dtype = np.uint8)
+        defect_contrast = np.random.randint(3,4, size = 1 , dtype = np.uint8)
         
         
         for col in range(x[0], x[0]+size_x[0]):
@@ -31,8 +31,8 @@ for image_num in range(0,100):
                 label_data[row][col] =255
         
      
-    #cv.imwrite("D:/WORKPLACE/make_image/test/" +str(image_num) + ".png",data)  
-    #cv.imwrite("D:/WORKPLACE/make_image/test/label/" +str(image_num) + ".png",label_data)    
-    cv.imwrite("D:/WORKPLACE/make_image/train/image/" +str(image_num) + ".png",data)
-    cv.imwrite("D:/WORKPLACE/make_image/train/label/" +str(image_num) + ".png",label_data)
+    cv.imwrite("D:/WORKPLACE/make_image/test/" +str(image_num) + ".png",data)  
+    cv.imwrite("D:/WORKPLACE/make_image/test/label/" +str(image_num) + ".png",label_data)    
+    #cv.imwrite("D:/WORKPLACE/make_image/train/image/" +str(image_num) + ".png",data)
+    #cv.imwrite("D:/WORKPLACE/make_image/train/label/" +str(image_num) + ".png",label_data)
    # cv.imshow("test",data)
